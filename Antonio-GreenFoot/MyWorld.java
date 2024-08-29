0bd25307d1e1aebca1bbdb1193bb6b494ca0923d
ANTONIO-2517 <antmoreno9@ielasnieves.edu.co>
2024-08-29 16:37:56 -0500

    GreenFoot Antonio 20/08/2008
    Trabajo en clase de greenfoot

---
+++ Antonio-GreenFoot/MyWorld.java
@@ -0,0 +1,22 @@
+import lang.stride.*;
+import greenfoot.*;
+import java.util.*;
+
+/**
+ * 
+ */
+public class MyWorld extends World
+{
+
+    /**
+     * Constructor for objects of class MyWorld.
+     */
+    public MyWorld()
+    {
+        super(600, 400, 1);
+        Delfin delfin = new Delfin(270);
+        addObject(delfin, 300, 300);
+        
+            }
+        }
+    
